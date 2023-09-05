54127e9d608d828d29991b9aa8b976f19a693acc
Sungwook Choi <sungwook.csw@gmail.com>
2023-09-05 21:21:02 +0900

    feat: Apply utils

--- store/lib/asyncUtils.ts
+++ store/lib/asyncUtils.ts
@@ -33,3 +33,28 @@ export const reducerUtils = {
     error,
   }),
 };
+
+export const handleAsyncActions = (type: string, key: string) => {
+  const [SUCCESS, ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`];
+  return (state: any, action: any) => {
+    switch (action.type) {
+      case type:
+        return {
+          ...state,
+          [key]: reducerUtils.loading(),
+        };
+      case SUCCESS:
+        return {
+          ...state,
+          [key]: reducerUtils.success(action.payload),
+        };
+      case ERROR:
+        return {
+          ...state,
+          [key]: reducerUtils.error(action.payload),
+        };
+      default:
+        return state;
+    }
+  };
+};
--- store/reducer/post.ts
+++ store/reducer/post.ts
@@ -1,4 +1,9 @@
 import * as postsAPI from "@/data/posts";
+import {
+  createPromiseThunk,
+  reducerUtils,
+  handleAsyncActions,
+} from "@/store/lib/asyncUtils";
 
 const GET_POSTS = "GET_POSTS"; // 요청 시작
 const GET_POSTS_SUCCESS = "GET_POSTS_SUCCESS"; // 요청 성공
@@ -8,98 +13,28 @@ const GET_POST = "GET_POST";
 const GET_POST_SUCCESS = "GET_POST_SUCCESS";
 const GET_POST_ERROR = "GET_POST_ERROR";
 
-const getPosts = () => async (dispatch: any) => {
-  dispatch({ type: GET_POSTS }); // 요청이 시작됨
-  try {
-    const posts = await postsAPI.getPosts(); // API 호출
-    dispatch({ type: GET_POSTS_SUCCESS, posts }); // 성공
-  } catch (e) {
-    dispatch({ type: GET_POSTS_ERROR, error: e }); // 실패
-  }
-};
-
-// thunk 함수에서도 파라미터를 받아와서 사용 할 수 있습니다.
-export const getPost = (id: number) => async (dispatch: any) => {
-  dispatch({ type: GET_POST }); // 요청이 시작됨
-  try {
-    const post = await postsAPI.getPostById(id); // API 호출
-    dispatch({ type: GET_POST_SUCCESS, post }); // 성공
-  } catch (e) {
-    dispatch({ type: GET_POST_ERROR, error: e }); // 실패
-  }
-};
+// thunk 함수
+const getPosts = () => createPromiseThunk("GET_POSTS", postsAPI.getPosts);
+const getPost = (id: number) =>
+  createPromiseThunk("GET_POST", postsAPI.getPostById);
 
 // 초기 상태 선언
 const initialState = {
-  posts: {
-    loading: false,
-    data: null,
-    error: null,
-  },
-  post: {
-    loading: false,
-    data: null,
-    error: null,
-  },
+  posts: reducerUtils.initial(),
+  post: reducerUtils.initial(),
 };
 
 // 리듀서 선언
-export default function posts(state = initialState, action) {
+export default function postsReducer(state = initialState, action: any) {
   switch (action.type) {
     case GET_POSTS:
-      return {
-        ...state,
-        posts: {
-          loading: true,
-          data: null,
-          error: null,
-        },
-      };
     case GET_POSTS_SUCCESS:
-      return {
-        ...state,
-        posts: {
-          loading: true,
-          data: action.posts,
-          error: null,
-        },
-      };
     case GET_POSTS_ERROR:
-      return {
-        ...state,
-        posts: {
-          loading: true,
-          data: null,
-          error: action.error,
-        },
-      };
+      return handleAsyncActions(GET_POSTS, "posts")(state, action);
     case GET_POST:
-      return {
-        ...state,
-        post: {
-          loading: true,
-          data: null,
-          error: null,
-        },
-      };
     case GET_POST_SUCCESS:
-      return {
-        ...state,
-        post: {
-          loading: true,
-          data: action.post,
-          error: null,
-        },
-      };
     case GET_POST_ERROR:
-      return {
-        ...state,
-        post: {
-          loading: true,
-          data: null,
-          error: action.error,
-        },
-      };
+      return handleAsyncActions(GET_POST, "post")(state, action);
     default:
       return state;
   }
